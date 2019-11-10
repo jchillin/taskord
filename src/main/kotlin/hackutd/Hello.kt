bf341bb6ea2e4524060f8a8a878c518b0e26eb20
Jacob <cooljacob.hill@gmail.com>
2019-11-10 00:41:44 -0600

    Database setup

--- src/main/kotlin/hackutd/Hello.kt
+++ src/main/kotlin/hackutd/Hello.kt
@@ -3,12 +3,13 @@ package hackutd
 import me.aberrantfox.kjdautils.api.dsl.PrefixDeleteMode
 import me.aberrantfox.kjdautils.api.startBot
 import org.jetbrains.exposed.dao.IntIdTable
-import org.jetbrains.exposed.sql.Database
-import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.transactions.transaction
+import javax.xml.validation.Schema
 
 
 fun main(args: Array<String>) {
-    Database.connect("jdbc:mysql://localhost:3306/test", driver = "com.mysql.jdbc.Driver",
+    Database.connect("jdbc:mysql://localhost:3306/taskord", driver = "com.mysql.cj.jdbc.Driver",
         user = dbUser, password = dbPassword)
     startBot(botToken) {
         configure {
@@ -18,6 +19,13 @@ fun main(args: Array<String>) {
             deleteMode = PrefixDeleteMode.None
         }
     }
+
+    transaction {
+        SchemaUtils.create(Tasks, Users)
+        
+    }
+
+
 }
 
 
@@ -25,6 +33,7 @@ object Tasks : IntIdTable() {
     val name = varchar("name", length = 255)
     val desc = text("desc").nullable()
     val user = reference("user",Users)
+    val deadline = datetime("deadline").nullable()
 }
 
 object Users : IntIdTable() {
