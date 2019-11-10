2dc191faba8c2956768a5130704c185e397db0a9
Jonathan Moore <programjm@gmail.com>
2019-11-09 21:52:14 -0600

    Add database tables

--- src/main/kotlin/hackutd/Hello.kt
+++ src/main/kotlin/hackutd/Hello.kt
@@ -2,9 +2,13 @@ package hackutd
 
 import me.aberrantfox.kjdautils.api.dsl.PrefixDeleteMode
 import me.aberrantfox.kjdautils.api.startBot
+import org.jetbrains.exposed.dao.IntIdTable
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.Table
 
 
 fun main(args: Array<String>) {
+    val db = Database.connect("jdbc:h2:mem:test", driver = "org.h2.Driver")
     startBot("NjQyOTA1ODMxMzEwMDk4NDM4.XcdyeA.cKgLXYq68QJKxKGTEGc_TlZx-ZY") {
         configure {
             prefix = "!"
@@ -15,3 +19,15 @@ fun main(args: Array<String>) {
     }
 }
 
+
+object Tasks : IntIdTable() {
+    val name = varchar("name", length = 255)
+    val desc = text("desc").nullable()
+    val user = reference("user",Users)
+}
+
+object Users : IntIdTable() {
+    val name = varchar("name", length=50)
+    val username = varchar("username", length=50)
+
+}
